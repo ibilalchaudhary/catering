54f70b4d8df71e20224206585ff6f84afd8e8c1f
BilalChaudhary7 <69617285+BilalChaudhary7@users.noreply.github.com>
2021-07-13 13:18:29 +0500

    social icons

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -3,7 +3,7 @@ import {View, Text, ImageBackground, TouchableOpacity} from 'react-native';
 import {withSafeAreaInsets} from 'react-native-safe-area-context';
 import {Height} from '../Constants/Constants';
 import {PRIMARY, SECONDAY, WHITE} from '../Constants/Colors';
-import Svg, {Path} from 'react-native-svg';
+import Svg, {G, Path} from 'react-native-svg';
 
 export default function HomeScreen() {
   return (
@@ -20,13 +20,19 @@ export default function HomeScreen() {
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          marginVertical: 40,
         }}>
         <TouchableOpacity
           style={{
+            width: '80%',
+            height: 50,
+            backgroundColor: '#4761BC',
+            borderRadius: 8,
             display: 'flex',
-            width: '75%',
-            height: 45,
-            backgroundColor: SECONDAY,
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: 20,
+            marginVertical: 10,
           }}>
           <Svg
             xmlns="http://www.w3.org/2000/svg"
@@ -39,7 +45,136 @@ export default function HomeScreen() {
               fill="#fff"
             />
           </Svg>
-          <Text>Continue with Facebook</Text>
+          <Text
+            style={{
+              fontSize: 14,
+              color: WHITE,
+              paddingHorizontal: 16,
+            }}>
+            Continue with Facebook
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            width: '80%',
+            height: 50,
+            backgroundColor: '#FF4B26',
+            borderRadius: 8,
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: 20,
+            marginVertical: 10,
+          }}>
+          <Svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={18.088}
+            height={18.088}
+            viewBox="0 0 18.088 18.088">
+            <G fill="#fff" data-name="g+">
+              <Path
+                data-name="Path 2116"
+                d="M17.406 7.471h-7.378a.59.59 0 00-.59.59v2.357a.59.59 0 00.59.59h4.155a5.546 5.546 0 01-2.388 2.8l1.772 3.067a8.868 8.868 0 004.522-7.756 5.979 5.979 0 00-.1-1.158.593.593 0 00-.583-.49z"
+              />
+              <Path
+                data-name="Path 2117"
+                d="M9.044 14.547a5.5 5.5 0 01-4.762-2.755L1.215 13.56a9.037 9.037 0 0012.351 3.315l-1.772-3.067a5.463 5.463 0 01-2.75.739z"
+              />
+              <Path
+                data-name="Path 2118"
+                d="M13.566 16.877l-1.772-3.067a5.464 5.464 0 01-2.75.744v3.539a9.029 9.029 0 004.522-1.216z"
+              />
+              <Path
+                data-name="Path 2119"
+                d="M3.539 9.044a5.464 5.464 0 01.743-2.75L1.215 4.526a9 9 0 000 9.036l3.067-1.768a5.464 5.464 0 01-.743-2.75z"
+              />
+              <Path
+                data-name="Path 2120"
+                d="M9.044 3.539a5.48 5.48 0 013.5 1.255.588.588 0 00.791-.036l1.67-1.67a.594.594 0 00-.034-.869 9.024 9.024 0 00-13.75 2.307l3.067 1.768a5.5 5.5 0 014.756-2.755z"
+              />
+              <Path
+                data-name="Path 2121"
+                d="M12.544 4.794a.588.588 0 00.791-.036l1.67-1.67a.594.594 0 00-.034-.869A9.017 9.017 0 009.044 0v3.539a5.48 5.48 0 013.5 1.255z"
+              />
+            </G>
+          </Svg>
+          <Text
+            style={{
+              fontSize: 14,
+              color: WHITE,
+              paddingHorizontal: 16,
+            }}>
+            Continue with Google
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            width: '80%',
+            height: 50,
+            backgroundColor: '#FFFFFF',
+            borderWidth: 1,
+            borderColor: '#707070',
+            borderRadius: 8,
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: 20,
+            marginVertical: 10,
+          }}>
+          <Svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={18.914}
+            height={13.299}
+            viewBox="0 0 18.914 13.299">
+            <G data-name={646135}>
+              <G data-name="Group 3">
+                <G data-name="Group 2">
+                  <Path
+                    data-name="Path 2122"
+                    d="M18.747.95l-5.736 5.7 5.736 5.7a1.643 1.643 0 00.167-.712V1.664a1.643 1.643 0 00-.167-.714z"
+                    fill="#110505"
+                  />
+                </G>
+              </G>
+              <G data-name="Group 5">
+                <G data-name="Group 4">
+                  <Path
+                    data-name="Path 2123"
+                    d="M17.251 0H1.662A1.643 1.643 0 00.95.167l7.331 7.295a1.664 1.664 0 002.351 0L17.963.167A1.643 1.643 0 0017.251 0z"
+                    fill="#110505"
+                  />
+                </G>
+              </G>
+              <G data-name="Group 7">
+                <G data-name="Group 6">
+                  <Path
+                    data-name="Path 2124"
+                    d="M.167.95A1.643 1.643 0 000 1.662v9.974a1.643 1.643 0 00.167.712l5.736-5.7z"
+                    fill="#110505"
+                  />
+                </G>
+              </G>
+              <G data-name="Group 9">
+                <G data-name="Group 8">
+                  <Path
+                    data-name="Path 2125"
+                    d="M12.229 7.433l-.812.812a2.773 2.773 0 01-3.918 0l-.812-.812-5.736 5.7a1.643 1.643 0 00.712.167h15.588a1.643 1.643 0 00.712-.167z"
+                    fill="#110505"
+                  />
+                </G>
+              </G>
+            </G>
+          </Svg>
+          <Text
+            style={{
+              fontSize: 14,
+              color: '#201A50',
+              paddingHorizontal: 16,
+            }}>
+            Continue as Guest
+          </Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
