a68ce0ba017155618d4f4aa50266db66a38e5b4f
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-07-22 21:17:59 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen';
 import StackNavigation from './Navigation/StackNavigation';
@@ -14,6 +14,7 @@ export default class App extends Component {
     return (
       <NavigationContainer>
         <SafeAreaView style={{flex: 1}}>
+          <StatusBar backgroundColor="black" />
           <StackNavigation />
         </SafeAreaView>
       </NavigationContainer>
